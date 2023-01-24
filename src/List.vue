5572bf952f3e2b31211b9f6db918c55cf5255721
DoomedAndria <103760839+DoomedAndria@users.noreply.github.com>
2023-01-24 19:11:23 +0400

    1

---
+++ src/List.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul class="ul">
+        <ListItem v-for="item in mylist" :key="item.id" :title="item.name"></ListItem>
+    </ul>
+</template>
+
+<script>
+import ListItem from './ListItem.vue';
+export default{
+    data() {
+        return {
+            mylist: [
+                {
+                    id: 1,
+                    name: "listItem 1"
+                },
+                {
+                    id: 2,
+                    name: "listItem 2"
+                },
+                {
+                    id: 3,
+                    name: "listItem 3"
+                }
+            ]
+        };
+    },
+    components: { ListItem }
+}
+</script>
+
+<style scoped>
+
+</style>
