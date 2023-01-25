86ef42b07bb35f6159577a5b599241d600ec3bcb
DoomedAndria <103760839+DoomedAndria@users.noreply.github.com>
2023-01-25 17:44:58 +0400

    heyo

--- src/HorisontalMenu.vue
+++ src/HorisontalMenu.vue
@@ -7,7 +7,7 @@ export default{
 <template>
     <div class="nav">
         <ul>
-            <li v-for="item in items" :key="item.id">  <a :href="item.url">{{ item.name }}</a> </li>
+            <li v-for="item in items" :key="item.id">  <a :href="item.url" target="_blank"> {{ item.name }} </a> </li>
         </ul>
     </div>
     
