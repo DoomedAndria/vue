a2916bac433b3cd184d5cd0ff8ba39eb206d8728
DoomedAndria <103760839+DoomedAndria@users.noreply.github.com>
2023-01-31 02:40:48 +0400

    api fetch ecom

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,46 @@
 <script >
-import ToDo from "./ToDo.vue"
-import Comp from "./Comp.vue"
+import Card from './components/Card.vue'
+import axios from 'axios'
 export default{
-
-  components: {ToDo,Comp},
-
+  components:{Card},
   data: function(){
     return {
-      todoList:[
-        {
-          id:1,
-          name: "todo item 1"
-        },
-        {
-          id:2,
-          name: "todo item 2"
-        },
-        {
-          id:3,
-          name: "todo item 3"
-        },
-        {
-          id:4,
-          name: "todo item 4"
-        },
-      ],
-
-      completedList:[
-
-      ]
+      url: 'https://items.magischer.de/api/products',
+      page: 1, 
+      limit: 4,
+      limits: [4,8,12,16,20],
+      lang:"en",
+      langs:["en","ge"],
+      next: null,
+      prev:null,
+      first:null,
+      last:null,
+      dta: []
     }
+  },
+  methods:{
+    getData: function(url = this.url){
+      axios.get(url,{
+        params:{
+          limit: this.limit,
+          lang: this.lang
+        }
+      }).then((response)=>{
+      this.dta = response.data.data
+
+      this.page = response.data.current_page
+
+      this.next = response.data.next_page_url
+      this.prev = response.data.prev_page_url
+      this.first = response.data.first_page_url
+      this.last = response.data.last_page_url
+      console.log(response.data)
+    })
+    },
+    
+  },
+  mounted() {
+    this.getData()
   },
   
 }
@@ -37,24 +48,109 @@ export default{
 
 <template>
   <div class="container">
-    <ToDo :items="todoList" :completed="completedList"></ToDo>
-    <Comp :items="todoList" :completed="completedList"></Comp>
+    <div class="cards">
+      <Card 
+        v-for="item in this.dta"
+        :img="item.img.cover"
+        :name="item.name"
+        :price="item.price"
+      >
+      </Card>
+    </div>
+    <div class="conf">
+      <div>
+        <button :disabled="this.first?false:true" @click="getData(this.first)">&lt;&lt;</button>
+        <button :disabled="this.prev?false:true" @click="getData(this.prev)">&lt;</button>
+        <button :disabled="this.next?false:true" @click="getData(this.next)">&gt;</button>
+        <button :disabled="this.last?false:true" @click="getData(this.last)">&gt;&gt;</button>
+      </div>
+
+      <br>
+
+      <p>item limit</p>
+      <select v-model="limit" @change="getData()">
+        <option v-for="l in limits" >{{l}}</option>
+      </select>
+
+      <br>
+
+      <p>language</p>
+      <select v-model="lang" @change="getData()">
+        <option v-for="l in langs" >{{l}}</option>
+      </select>
+
+      <h1>PAGE: {{this.page}}</h1>
+    </div>
   </div>
  
 </template>
 
 <style scoped>
+
   .container{
     display: flex;
     flex-direction: row;
-    justify-content: center;
-    align-items: center;
+   
     font-family: 'Quicksand', sans-serif;
     font-weight: 600;
-    background-color: rgba(29, 96, 93, 0.164);
-    width: 100vw;
-    height: 100vh;
-    gap: 20px;
+    background-color: rgba(29, 96, 93, 0.062);
+    min-width: 100vw;
+    min-height: 100vh;
+  }
+
+  .cards{
+    width: 80%;
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+
+  .conf{
+    display: flex;
+    flex-direction: column;
+    color: rgba(0, 0, 255, 0.679);
+  }
+
+  .conf select{
+    outline: none;
+    border: none;
+    height: 30px;
+    background-color: rgba(211, 211, 211, 0.55);
+    border-radius: 5px;
+    font-family: 'Quicksand', sans-serif;
+    font-weight: 900;
+    color: darkslategrey;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    cursor: pointer;
+  }
+
+  .conf div{
+    padding-top: 50px;
+    display: flex;
+    gap: 5px;
+  }
+ 
+  .conf button{
+    font-size: 16px;
+    padding: 10px 10px;
+    font-family: 'Quicksand', sans-serif;
+    font-weight: 900;
+    text-transform: uppercase;
+    cursor: pointer;
+    border: none;
+    background-color: rgba(32, 178, 171, 0.613);
+    color: white;
+    border-radius: 5px;
+    transition: all 0.25s;
+  }
+
+  .conf button:hover{
+    background-color: lightseagreen;
+  }
+
+  .conf button:disabled{
+    background-color: lightgray;
   }
 
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,88 @@
+<script>
+export default{
+    props:{
+        img:String,
+        name:String,
+        price:Number,
+    }
+}
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="img">
+            <img :src="img">
+        </div>
+        <div class="name">
+            {{ this.name }}
+        </div>
+        <div class="price">
+            {{ this.price }} ₾
+            <button class="cart">Add <i class="gg-shopping-cart"></i></button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+@import url('https://css.gg/shopping-cart.css');
+    .card{
+        width: 250px;
+        height: 300px;
+        margin: 10px;
+        padding: 10px;
+        border-radius: 10px;
+        box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+        background-color: rgba(108, 206, 152, 0.421);
+        color: darkslategrey;
+        text-align: center;
+        
+    }
+    .img{
+        height: 170px;
+        width: 100%;
+    }
+
+    .img img{
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+        border-radius: 7px;
+        box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+    }
+    .name{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 80px;
+        font-size: 22px;
+    }
+    .price{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 30px;
+        font-size: 18px;
+        color: rgba(220, 20, 60, 0.791);
+    }
+    .cart{
+        display: flex;
+        gap: 5px;
+        font-size: 16px;
+        padding: 10px 10px;
+        font-family: 'Quicksand', sans-serif;
+        font-weight: 900;
+        text-transform: uppercase;
+        cursor: pointer;
+        border: none;
+        background-color: rgba(60, 179, 114, 0.665);
+        color: white;
+        border-radius: 5px;
+        transition: all 0.25s;
+    }
+
+    .cart:hover{
+        background-color: rgb(60, 179, 114);
+    }
+</style>
